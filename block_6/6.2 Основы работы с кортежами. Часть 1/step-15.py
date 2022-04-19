760079605122f9671e6ce137b3615b1db331c38f
ArtemSweb <solodovnikov-artem@inbox.ru>
2022-04-19 18:09:48 +0300

    шаг 15

---
+++ block_6/6.2 Основы работы с кортежами. Часть 1/step-15.py
@@ -0,0 +1,10 @@
+tuples = [(), (), ('',), ('a', 'b'), (), ('a', 'b', 'c'), (1,), (), (), ('d',), ('', ''), ()]
+non_empty_tuples = [(i) for i in tuples if len(i)>0]
+
+print(non_empty_tuples)
+
+
+
+# for i in tuples:
+# 	if len(i) > 0:
+# 		non_empty_tuples.append(i)
