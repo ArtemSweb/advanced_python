978818f00f0dbf9c8936a81332319d4dc641e96c
ArtemSweb <solodovnikov-artem@inbox.ru>
2022-05-07 12:05:31 +0300

    вариант решения 10.2.13

---
+++ block_8/block_10/10.2 Основы работы со словарями/step-13.py
@@ -0,0 +1,17 @@
+d = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine"
+}
+
+lst = [int(i) for i in input()]
+
+for i in lst:
+	print(d[i], end=' ')
