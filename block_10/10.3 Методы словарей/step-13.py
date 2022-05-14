f6761ea63f77f81fd5e7b63b60b773f3a15f86c4
ArtemSweb <solodovnikov-artem@inbox.ru>
2022-05-14 22:16:02 +0300

    вариант решения 10.3.13

---
+++ block_10/10.3 Методы словарей/step-13.py
@@ -0,0 +1,10 @@
+from cgitb import reset
+
+
+s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
+result = {}
+
+for word in s.split():
+    result[word] = result.get(word, 0) + 1
+
+print(sorted(i for i in result if result[i]==max(result.values()))[0])
