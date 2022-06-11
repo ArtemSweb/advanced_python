c7d9cfb83b92750bbffcfc2577c5f421023f3d00
ArtemSweb <solodovnikov-artem@inbox.ru>
2022-06-11 12:24:21 +0300

    Юный математик

---
+++ block_13/13.2 Модуль fraction/step-12.py
@@ -0,0 +1,12 @@
+"""Юный математик 🌶️"""
+
+from fractions import Fraction as F
+from math import gcd
+
+n = int(input())
+a = n // 2
+b = n - a
+while gcd(a, b) != 1:
+    a -= 1
+    b += 1
+print(F(a, b))
