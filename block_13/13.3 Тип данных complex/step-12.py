e61c0dc4b813f7eda1fbf942fb84f3ccd1ae98c8
ArtemSweb <solodovnikov-artem@inbox.ru>
2022-06-11 17:55:56 +0300

    13.3.12

---
+++ block_13/13.3 Тип данных complex/step-12.py
@@ -0,0 +1,7 @@
+"""Операции над комлексными числами"""
+
+z1, z2 = complex(input()), complex(input())
+
+print(z1, '+', z2, '=', z1 + z2)
+print(z1, '-', z2, '=', z1 - z2)
+print(z1, '*', z2, '=', z1 * z2)
