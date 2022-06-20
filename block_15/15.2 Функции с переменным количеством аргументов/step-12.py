4359ad27819fa3a557986ba4f5a5b32721621782
ArtemSweb <solodovnikov-artem@inbox.ru>
2022-06-20 17:32:53 +0300

    test

--- block_15/15.2 Функции с переменным количеством аргументов/step-12.py
+++ block_15/15.2 Функции с переменным количеством аргументов/step-12.py
@@ -1,9 +1,12 @@
 
 
 def mean(*args):
-	return sum(args)/len(args)
+    return sum(args)/len(args)
+    
 
 print(mean())
 print(mean(7))
-print(mean(7))
-print(mean(7))
+print(mean(1.5, True, ['stepik'], 'beegeek', 2.5, (1, 2)))
+print(mean(True, ['stepik'], 'beegeek', (1, 2)))
+print(mean(-1, 2, 3, 10, ('5')))
+print(mean(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
