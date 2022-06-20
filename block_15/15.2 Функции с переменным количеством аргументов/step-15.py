f84f272cbacbf80f0d09e4738cd4b958db8ea76d
ArtemSweb <solodovnikov-artem@inbox.ru>
2022-06-20 23:30:22 +0300

    вариант решения 15.2.15

---
+++ block_15/15.2 Функции с переменным количеством аргументов/step-15.py
@@ -0,0 +1,8 @@
+
+
+def info_kwargs(**kwargs):
+    for key, value in sorted(kwargs.items()):
+        print(key + ": " + str(value))
+
+
+info_kwargs(first_name='Timur', last_name='Guev', age=28, job='teacher') 
