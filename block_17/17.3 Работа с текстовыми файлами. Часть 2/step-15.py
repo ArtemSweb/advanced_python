34a23380411e497646973b4722a397783876f445
ArtemSweb <solodovnikov-artem@inbox.ru>
2022-07-05 23:46:32 +0300

    вариант решения 17.3.15

---
+++ block_17/17.3 Работа с текстовыми файлами. Часть 2/step-15.py
@@ -0,0 +1,8 @@
+"""CSV-файл"""
+
+def read_csv():
+    with open('data.csv', encoding="utf-8") as file:
+        keys = file.readline().strip().split(',')
+        return [dict(zip(keys, line.strip().split(','))) for line in file]
+
+print(read_csv())
